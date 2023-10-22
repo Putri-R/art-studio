a5ca0f18ebb81914f41bc59884f20ca7d3fd4d1e
Putri-R <putrirahmadhani005@gmail.com>
2023-10-22 10:38:33 +0700

    Implementasi landing page

---
+++ Landing Page/landing_page/src/components/Hero Page/heroPage.jsx
@@ -0,0 +1,23 @@
+import Navigasi from "../Navigasi/navigasi";
+import './hero_style.css';
+import hero_img from './asset/hero_image.png'
+
+function Heropage(){
+    return(
+        <>
+            <Navigasi/>
+            <div className="hero">
+                <div className="hero_group">
+                    <h1>Unleashing and nurturing the <span>great potential</span> you possess</h1>
+                    <p>Let's learn together with enjoyable instructors in a comfortable environment to discover and develop your artistic potential</p>
+                    <button className="hero-button" type="button">Get Started</button>
+                </div>
+                <div className="hero_group">
+                    <img src={hero_img} alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Heropage;
