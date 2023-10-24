4baba3083abb272fb31d37b5b3d9f3e174603826
Putri-R <putrirahmadhani005@gmail.com>
2023-10-24 16:08:29 +0700

    Update Development

--- Development/art-studio/src/App.js
+++ Development/art-studio/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/Landing Page/landingPage';
+import ContactUs from './pages/Contact Us/contactUs';
+import AboutUs from './pages/About Us/aboutUs';
+import Event1 from './pages/Event1 Page/event1Page';
+import Event2 from './pages/Event2 Page/event2Page';
+import Event3 from './pages/Event3 Page/event3Page';
+import AllEventPage from './pages/All Event Page/allEventPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/contact-us" element={<ContactUs/>} />
+        <Route path="/" element={<LandingPage/>} />
+        <Route path="/about-us" element={<AboutUs/>} />
+        <Route path="/music-competition" element={<Event1/>} />
+        <Route path="/dance-competition" element={<Event2/>} />
+        <Route path="/painting-competition" element={<Event3/>} />
+        <Route path="/more-event" element={<AllEventPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
